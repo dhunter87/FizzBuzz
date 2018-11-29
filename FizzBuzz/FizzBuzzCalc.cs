1cab9b2a5e03352d3595a30e2ef93a355e48a130
Dan Hunter <dan.hunter55@yahoo.com>
2018-11-29 17:45:45 +0000

    completed 1st code challenge, i think!

---
+++ FizzBuzz/FizzBuzzCalc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzCalc
+    {
+        public static string Get(int n)
+        {
+            /*
+            string result = n.ToString(); ;
+            return result;
+            */
+            if (n % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (n % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return n.ToString();
+            }
+        }
+    }
+}
---
+++ FizzBuzzUnitTest/FizzBuzzTests.cs
@@ -0,0 +1,31 @@
+﻿using FizzBuzz;
+using NUnit.Framework;
+
+namespace FizzBuzzUnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var fizBuzz = new FizzBuzzCalc();
+
+
+            string result = FizzBuzzCalc.Get(1);
+
+            Assert.That(result, Is.EqualTo("1"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var engine = new FizzBuzzCalc();
+
+            const int input = 2;
+            string output = FizzBuzzCalc.Get(input);
+
+            Assert.That(output, Is.EqualTo("2"));
+        }
+    }
+}
